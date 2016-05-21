bdc265915766d974bc708ea08ce2d5018c9a3a2c
bravo-kernel <bravo-kernel@users.noreply.github.com>
2016-05-21 15:25:34 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # cakephp-validation-exposer
-Easily expose your application validation rules
+
+[![Build Status](https://img.shields.io/travis/alt3/cakephp-validation-exposer/master.svg?style=flat-square)](https://travis-ci.org/alt3/cakephp-validation-exposer)
+[![StyleCI Status](https://styleci.io/repos/45741948/shield)](https://styleci.io/repos/45741948)
+[![Coverage](https://img.shields.io/coveralls/alt3/cakephp-validation-exposer/master.svg?style=flat-square)](https://coveralls.io/r/alt3/cakephp-validation-exposer)
+[![Total Downloads](https://img.shields.io/packagist/dt/alt3/cakephp-validation-exposer.svg?style=flat-square)](https://packagist.org/packages/alt3/cakephp-validation-exposer)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)](LICENSE.txt)
+
+CakePHP 3.x plugin to easily expose your application validation rules.
+
+## Requirements
+
+* CakePHP 3.0+
+
+## Installation
+
+1. Install the plugin using composer:
+
+    ```bash
+    composer require alt3/cakephp-validation-exposer:dev-master
+    ```
+
+2. To enable the plugin either run the following command:
+
+    ```bash
+    bin/cake plugin load Alt3/ValidationExposer
+    ```
+
+    or manually add the following line to your `config/bootstrap.php` file:
+
+    ```bash
+    Plugin::load('Alt3/ValidationExposer');
+    ```
+
+## Contribute
+
+Before submitting a PR make sure:
+
+- [PHPUnit](http://book.cakephp.org/3.0/en/development/testing.html#running-tests)
+and [CakePHP Code Sniffer](https://github.com/cakephp/cakephp-codesniffer) tests pass
+- [Coveralls Code Coverage ](https://coveralls.io/github/alt3/cakephp-validation-exposer) remains at 100%
---
+++ src/Lib/ValidationExposer.php
@@ -0,0 +1,171 @@
+<?php
+namespace Alt3\ValidationExposer;
+
+use Cake\Datasource\ConnectionManager;
+use Cake\Network\Exception\InternalErrorException;
+use Cake\ORM\TableRegistry;
+use Cake\Utility\Hash;
+
+/**
+ * Class library to help expose a list of all validation rules in an application.
+ */
+class ValidationExposer
+{
+    /**
+     * Array with merged configuration settings.
+     *
+     * @var array
+     */
+    protected $_config = [
+        'exclude' => [
+            'phinxlog'
+        ]
+    ];
+
+    /**
+     * Flat array holding all tables found in the application, minus exclude.
+     *
+     * @var array
+     */
+    protected $_tables;
+
+    /**
+     * @var string $_exclude Flat array with table names to exclude.
+     */
+    protected $_exclude;
+
+    /**
+     * Class constructor.
+     *
+     * @param array $config Configuration options
+     */
+    public function __construct($config = [])
+    {
+        $this->_config = Hash::merge($this->_config, $config);
+        $this->_exclude = $this->_config['exclude'];
+        $this->_tables = $this->_getApplicationTables();
+    }
+
+    /**
+     * Returns an array with all tables included in validation rule aggregation.
+     *
+     * @return array
+     */
+    public function applicationTables()
+    {
+        return $this->_tables;
+    }
+
+    /**
+     * Returns an array with all tables excluded from validation rule aggregation.
+     *
+     * @return mixed|string
+     */
+    public function excludedTables()
+    {
+        return $this->_exclude;
+    }
+
+    /**
+     * Returns an array with validation rules for all tables.
+     *
+     * @return array Array holding validation rules
+     */
+    public function applicationRules()
+    {
+        // process rules per table
+        $result = [];
+        foreach ($this->_tables as $key => $table) {
+            $tableRules = $this->_getTableValidationRules($table);
+            if (count($tableRules)) {
+                $result[$table] = $tableRules;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Returns a flat array with lowercased/underscored names for all tables
+     * found in the application, minus configuration excluded tables.
+     *
+     * return void
+     */
+    protected function _getApplicationTables()
+    {
+        $tables = ConnectionManager::get('default')->schemaCollection()->listTables();
+        if (!count($tables)) {
+            throw new InternalErrorException("Could not find any tables in the application");
+        }
+
+        if(!count($this->_exclude)) {
+            return $tables;
+        }
+
+        return array_diff($tables, $this->_exclude);
+    }
+
+    /**
+     * Returns a hash with all validation rules for any given table.
+     *
+     * @param $table Valid table name
+     * @return array|void Hash if table contains validation rules, void otherwise
+     */
+    protected function _getTableValidationRules($table) {
+        $tableObject = TableRegistry::get($table);
+        $validator = $tableObject->validator();
+        $validationSetIterator = $validator->getIterator();
+
+        if (!$validationSetIterator->count()) {
+            return;
+        }
+
+        $result = [];
+
+        foreach ($validationSetIterator as $field => $validationSet) {
+            $requiredFor = $validationSet->isPresenceRequired();
+            if (!$requiredFor) {
+                $requiredFor = null;
+            }
+            $result[$field]['requiredFor'] = $requiredFor;
+
+            $allowedEmptyFor = $validationSet->isEmptyAllowed();
+            if (!$allowedEmptyFor) {
+                $allowedEmptyFor = null;
+            }
+            $result[$field]['allowedEmptyFor'] = $allowedEmptyFor;
+
+            // continue foreach if field has no rules
+            $validationRuleIterator = $validationSet->getIterator();
+            if (!$validationRuleIterator->count()) {
+                continue;
+            }
+
+            // add create rules array to store
+            $result[$field]['rules'] = [];
+
+            foreach ($validationRuleIterator as $ruleName => $validationRule) {
+                $rule = $validationRule->get('rule');
+                $message = $validationRule->get('message');
+                $pass = $validationRule->get('pass');
+
+                if (!empty($pass)) {
+                    $result[$field]['rules'][] = [
+                        'name' => $ruleName,
+                        'rule' => $rule,
+                        'message' => $message,
+                        'pass' => $pass
+                    ];
+                } else {
+                    $result[$field]['rules'][] = [
+                        'name' => $ruleName,
+                        'rule' => $rule,
+                        'message' => $message
+                    ];
+                }
+            }
+        }
+
+        return $result;
+    }
+}
